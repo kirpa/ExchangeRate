af1d03b5e8f786056e30a25488cc4d45a99aeb29
Vadim Zhepetov <vadim.zhepetov@scandiweb.com>
2019-10-08 17:08:16 +0300

    Initial

---
+++ ExchangeRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRates
+//
+//  Created by Vadim Zhepetov on 08/10/2019.
+//  Copyright © 2019 Vadim Zhepetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
